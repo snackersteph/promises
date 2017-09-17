be85f6af1682fe35c22746f50d552740019641cd
Stephanie Wong <swongwrites@gmail.com>
2017-09-16 17:20:46 -0700

    Complete promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,6 +8,9 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
+var funcPromise = require('./promisification.js');
+var funcCallback = require('./callbackReview.js');
+var funcConstructor = require('./promiseConstructor.js')
 var fs = require('fs');
 var Promise = require('bluebird');
 
@@ -15,7 +18,24 @@ var Promise = require('bluebird');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
-};
+  return new Promise((resolve, reject) => {
+    return funcConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then((username) => {
+      return funcPromise.getGitHubProfileAsync(username) 
+    }).then((profile) => {
+      fs.writeFile(writeFilePath, JSON.stringify(profile), (err) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
+    })
+  })
+}
+
+
+
 
 // Export these functions so we can test them
 module.exports = {
--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -8,11 +8,25 @@ var request = require('request');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      arguments[1](err);
+    } else {
+      arguments[1](err, data.toString().split('\n')[0]);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url) {
   // TODO
+  request(url, (err, response, body) => {
+    if (err) {
+      arguments[1](err);
+    } else {
+      arguments[1](err, response.statusCode);
+    }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
